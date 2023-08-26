614b45fef525c05c95b355308cc3802eaee9833d
Gregorio, Ace Russell A <113320302+gregorioacerussell@users.noreply.github.com>
2023-08-26 12:46:08 +0800

    Add files via upload

---
+++ 6.11 Exercises/(1A) 6.11 Exercises.py
@@ -0,0 +1,7 @@
+# 1. Write a program that asks the user to enter a string. The program should then print the
+# following:
+
+# a.) The total number of characters in the string
+
+string = input("Enter anything: ")
+print(f"The number of characters in the string {string} is: {len(string)}.")
